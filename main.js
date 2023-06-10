9ed0686f8946afe3e3ce4e8b2d7828eacd23f982
yurika12345 <yurikuru0511@gmail.com>
2023-06-11 03:06:57 +0900

    0610

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
-  //絞り込み一覧
-document.getElementById('searchBox').addEventListener('click', function() {
-    var dropdown = document.getElementById('dropdown');
-    if (dropdown.style.display === 'none' || dropdown.style.display === '') {
-      dropdown.style.display = 'block';
-    } else {
-      dropdown.style.display = 'none';
-    }
-  });
+//絞り込み一覧
+document.getElementById('searchBox').addEventListener('click', function () {
+  var dropdown = document.getElementById('dropdown');
+  if (dropdown.style.display === 'none' || dropdown.style.display === '') {
+    dropdown.style.display = 'block';
+  } else {
+    dropdown.style.display = 'none';
+  }
+});
 
-  function search() {
-    // チェックボックスの状態を取得
-    var price1Checked = document.getElementById('price1').checked;
-    var price2Checked = document.getElementById('price2').checked;
-    
-    // チェックボックスの状態をlocalStorageに保存
-    localStorage.setItem('price1', price1Checked);
-    localStorage.setItem('price2', price2Checked);
-    
-    // 検索結果のページに移動
-    window.location.href = 'products.html';
+function search() {
+  // チェックボックスの状態を取得
+  var price1Checked = document.getElementById('price1').checked;
+  var price2Checked = document.getElementById('price2').checked;
+  var price3Checked = document.getElementById('price3').checked;
+  var price4Checked = document.getElementById('price4').checked;
+
+  var scene1Checked = document.getElementById('scene1').checked;
+  var scene2Checked = document.getElementById('scene2').checked;
+  var scene3Checked = document.getElementById('scene3').checked;
+  var scene4Checked = document.getElementById('scene4').checked;
+
+  var feature1Checked = document.getElementById('feature1').checked;
+  var feature2Checked = document.getElementById('feature2').checked;
+  var feature3Checked = document.getElementById('feature3').checked;
+
+  // チェックボックスの状態をlocalStorageに保存
+  localStorage.setItem('price1', price1Checked);
+  localStorage.setItem('price2', price2Checked);
+  localStorage.setItem('price3', price3Checked);
+  localStorage.setItem('price4', price4Checked);
+
+  localStorage.setItem('scene1', scene1Checked);
+  localStorage.setItem('scene2', scene2Checked);
+  localStorage.setItem('scene3', scene3Checked);
+  localStorage.setItem('scene4', scene4Checked);
+
+  localStorage.setItem('feature1', feature1Checked);
+  localStorage.setItem('feature2', feature2Checked);
+  localStorage.setItem('feature3', feature3Checked);
+
+  // 検索結果のページに移動
+  window.location.href = 'products.html';
 }
--- products.js
+++ products.js
@@ -1,20 +1,97 @@
-window.onload = function() {
+window.onload = function () {
     // 商品の表示領域を取得
     var productsDiv = document.getElementById('products');
-    
+
     // localStorageからチェックボックスの状態を取得
     var price1Checked = localStorage.getItem('price1') === 'true';
     var price2Checked = localStorage.getItem('price2') === 'true';
-    
+    var price3Checked = localStorage.getItem('price3') === 'true';
+    var price4Checked = localStorage.getItem('price4') === 'true';
+
+    var scene1Checked = localStorage.getItem('scene1') === 'true';
+    var scene2Checked = localStorage.getItem('scene2') === 'true';
+    var scene3Checked = localStorage.getItem('scene3') === 'true';
+    var scene4Checked = localStorage.getItem('scene4') === 'true';
+
+    var feature1Checked = localStorage.getItem('feature1') === 'true';
+    var feature2Checked = localStorage.getItem('feature2') === 'true';
+    var feature3Checked = localStorage.getItem('feature3') === 'true';
+
+
     // チェックボックスの状態に基づいて表示する商品を決定
-    if (price1Checked) {
-        var productA = document.getElementById('productA');
-        productA.style.display = "block";  // 商品Aを表示に設定
-        productsDiv.appendChild += productA;  // 商品Aを表示
-    }
-    if (price2Checked) {
-        var productB = document.getElementById('productB');
-        productB.style.display = "block";  // 商品Bを表示に設定
-        productsDiv.appendChild += productB;  // 商品Bを表示
-    }
+        
+        if (price1Checked) {
+            var productA = document.getElementById('productA');
+            productA.style.display = "block";  // 商品Aを表示に設定
+            productsDiv.appendChild (productA);  // 商品Aを表示
+        }
+
+        if (price2Checked) {
+            var productB = document.getElementById('productB');
+            productB.style.display = "block";  // 商品Bを表示に設定
+        
+            var productB2 = document.getElementById('productB-2');
+            productB2.style.display = "block";  // 商品B-2を表示に設定
+        
+            // 仮定：productsDivは既存の親要素を指す
+            productsDiv.appendChild(productB);  // 商品Bを表示
+            productsDiv.appendChild(productB2); // 商品B-2を表示
+        }
+
+        if (price3Checked) {
+            var productC = document.getElementById('productC');
+            productC.style.display = "block";  
+            productsDiv.appendChild (productC); 
+        }
+
+        if (price4Checked) {
+            var productD = document.getElementById('productD');
+            productD.style.display = "block";  
+            productsDiv.appendChild (productD); 
+        }
+
+        if (scene1Checked) {
+            var productE = document.getElementById('productE');
+            productE.style.display = "block";  
+            productsDiv.appendChild (productE); 
+        }
+
+        if (scene2Checked) {
+            var productF = document.getElementById('productF');
+            productF.style.display = "block";  
+            productsDiv.appendChild (productF); 
+        }
+
+        if (scene3Checked) {
+            var productG = document.getElementById('productG');
+            productG.style.display = "block";  
+            productsDiv.appendChild (productG); 
+        }
+
+        if (scene4Checked) {
+            var productH = document.getElementById('productH');
+            productH.style.display = "block";  
+            productsDiv.appendChild (productH); 
+        }
+
+        if (feature1Checked) {
+            var productI = document.getElementById('productI');
+            productI.style.display = "block";  
+            productsDiv.appendChild (productI); 
+        }
+
+        if (feature2Checked) {
+            var productJ = document.getElementById('productJ');
+            productJ.style.display = "block";  
+            productsDiv.appendChild (productJ); 
+        }
+
+        if (feature3Checked) {
+            var productK = document.getElementById('productK');
+            productK.style.display = "block";  
+            productsDiv.appendChild (productK); 
+        }
+
+   
+
 }
